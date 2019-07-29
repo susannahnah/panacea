0cc92fbb519dfb9baf690eb81003ac9087732167
dayton brock <dayton.brock@gmail.com>
2019-07-29 16:57:01 -0500

    add user and admin page routes to App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,17 +7,20 @@ import {
 } from 'react-router-dom';
 
 import {connect} from 'react-redux';
-
-import Nav from '../Nav/Nav';
-import Footer from '../Footer/Footer';
-
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute'
-
-import AboutPage from '../AboutPage/AboutPage';
-import UserPage from '../UserPage/UserPage';
-import InfoPage from '../InfoPage/InfoPage';
-
 import './App.css';
+
+// Components
+import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+import UserLandingPage from '../user/UserLandingPage/UserLandingPage';
+import CityPage from '../user/CityPage/CityPage';
+import MedicationsPage from '../user/MedicationsPage/MedicationsPage';
+import MapPage from '../user/MapPage/MapPage';
+import OrganizationPage from '../user/OrganizationPage/OrganizationPage';
+import AdminLandingPage from '../admin/AdminLandingPage/AdminLandingPage';
+import CitiesPage from '../admin/CitiesPage/CitiesPage';
+import CityFormPage from '../admin/CityFormPage/CityFormPage';
+import OrganizationsPage from '../admin/OrganizationsPage/OrganizationsPage';
+import OrganizationFormPage from '../admin/OrganizationFormPage/OrganizationFormPage';
 
 class App extends Component {
   componentDidMount () {
@@ -28,37 +31,23 @@ class App extends Component {
     return (
       <Router>
         <div>
-          <Nav />
           <Switch>
-            {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
-            <Redirect exact from="/" to="/home" />
-            {/* Visiting localhost:3000/about will show the about page.
-            This is a route anyone can see, no login necessary */}
-            <Route
-              exact
-              path="/about"
-              component={AboutPage}
-            />
-            {/* For protected routes, the view could show one of several things on the same route.
-            Visiting localhost:3000/home will show the UserPage if the user is logged in.
-            If the user is not logged in, the ProtectedRoute will show the 'Login' or 'Register' page.
-            Even though it seems like they are different pages, the user is always on localhost:3000/home */}
-            <ProtectedRoute
-              exact
-              path="/home"
-              component={UserPage}
-            />
-            {/* This works the same as the other protected route, except that if the user is logged in,
-            they will see the info page instead. */}
-            <ProtectedRoute
-              exact
-              path="/info"
-              component={InfoPage}
-            />
+            {/* User Pages */}
+            <Route exact path="/" component={UserLandingPage}/>
+            <Route exact path="/:city-name" component={CityPage}/>
+            <Route exact path="/:city-name/medications" component={MedicationsPage}/>
+            <Route exact path="/:city-name/map" component={MapPage}/>
+            <Route exact path="/:city-name/:org-name" component={OrganizationPage}/>
+            {/* "Not Found" Page if user attempts to search for a city not in the database */}
+            {/* Admin Pages */}
+            <ProtectedRoute exact path="/admin" component={AdminLandingPage}/>
+            <ProtectedRoute exact path="/cities" component={CitiesPage}/>
+            <ProtectedRoute exact path="/cities/:city-name" component={CityFormPage}/>
+            <ProtectedRoute exact path="/organizations" component={OrganizationsPage}/>
+            <ProtectedRoute exact path="/organizations/:org-name" component={OrganizationFormPage}/>
             {/* If none of the other routes matched, we will show a 404. */}
             <Route render={() => <h1>404</h1>} />
           </Switch>
-          <Footer />
         </div>
       </Router>
   )}
