3ac5f939c45c6ba373f0701ef7ea7705bb345c3d
Koua Xiong <kxiong517@gmail.com>
2019-07-31 10:34:22 -0500

    working search for cities

--- src/redux/reducers/searchReducer.js
+++ src/redux/reducers/searchReducer.js
@@ -1,3 +1,14 @@
+import { combineReducers } from 'redux';
+
+const searchCityReducer = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_CITY_SEARCH_RESULT':
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
 const searchOrganizationReducer = (state = [], action) => {
     switch (action.type) {
         case 'SET_ORGANIZATION_SEARCH_RESULT':
@@ -7,4 +18,9 @@ const searchOrganizationReducer = (state = [], action) => {
     }
 }
 
-export default searchOrganizationReducer;
+const searchReducer = combineReducers({
+    searchCityReducer,
+    searchOrganizationReducer,
+});
+
+export default searchReducer;
--- src/redux/sagas/searchSaga.js
+++ src/redux/sagas/searchSaga.js
@@ -1,20 +1,25 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import axios from 'axios';
 
-function* searchCitySaga(action){
-    
+function* searchCitySaga(action) {
+    try {
+        const { data } = yield axios.get(`/api/search/city?city_name=%${action.payload}%`);
+        yield put({ type: 'SET_CITY_SEARCH_RESULT', payload: data });
+    } catch (error) {
+        console.log('Error with search city saga:', error);
+    }
 }
 
-function* searchOrganizationSaga(action){
+function* searchOrganizationSaga(action) {
     try {
         const { data } = yield axios.get(`/api/search/organization?organization_name=%${action.payload}%`);
-        yield put({type: 'SET_ORGANIZATION_SEARCH_RESULT', payload: data});
+        yield put({ type: 'SET_ORGANIZATION_SEARCH_RESULT', payload: data });
     } catch (error) {
         console.log('Error with search org saga:', error);
     }
 }
 
-function* searchSaga(){
+function* searchSaga() {
     yield takeEvery('SEARCH_ORGANIZATION', searchOrganizationSaga);
     yield takeEvery('SEARCH_CITY', searchCitySaga);
 }
