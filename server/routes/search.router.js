84a079df693420e03c7dae168a7254a58e387c57
Koua Xiong <kxiong517@gmail.com>
2019-07-31 10:55:21 -0500

    comments to help explain some backend code

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const pool = require('../modules/pool');
 
-router.get('/city', async(req, res)=>{
+router.get('/city', async(req, res)=>{ // async is just another way of writing promises like .then
     try {
-        const { city_name } = req.query;
+        const { city_name } = req.query; // destructuring the query object only looking for the city name
         const searchQuery = 'SELECT * FROM "cities" WHERE "name" ILIKE $1';
-        const { rows } = await pool.query(searchQuery, [city_name]);
+        const { rows } = await pool.query(searchQuery, [city_name]); // destructuring the result only looking for the rows
         await res.send(rows);
     } catch (error) {
         throw error;
@@ -14,9 +14,9 @@ router.get('/city', async(req, res)=>{
 
 router.get('/organization', async(req, res) => {
     try {
-        const { organization_name } = req.query; // destructuring the query object only looking for the org name
+        const { organization_name } = req.query; 
         const searchQuery = 'SELECT * FROM "organizations" WHERE "name" ILIKE $1';
-        const { rows } = await pool.query(searchQuery, [organization_name]); // destructuring the result only looking for the rows
+        const { rows } = await pool.query(searchQuery, [organization_name]); 
         await res.send(rows);
     } catch (error) {
         throw error;
