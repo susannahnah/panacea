67fb4c9916bb4f4fd7d8473bfccf9c75057b25a1
John Gellert <gellertjm@gmail.com>
2019-08-01 13:15:52 -0500

    Add styling to buttons and textfields.

--- src/components/admin/CitiesPage/CitiesPage.js
+++ src/components/admin/CitiesPage/CitiesPage.js
@@ -1,93 +1,118 @@
-import React, { Component } from "react";
+import React from "react";
 
 // Material-UI
 import Button from "@material-ui/core/Button";
 import { TextField } from "@material-ui/core";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import { blue } from "@material-ui/core/colors";
 
-class CitiesPage extends Component {
+const useStyles = makeStyles({
+  addButton: {
+    height: 75,
+    color: "white",
+    background: "#76a3d5",
+  },
+  searchButton: {
+    height: 50,
+    color: "white",
+    background: "#76a3d5",
+  }
+});
+
+function CitiesPage() {
   // Local state to store inputs for city and country to search.
-  state = {
+  const classes = useStyles();
+
+  const [searchValues, setSearchValues] = React.useState({
     city: "",
     country: ""
-  };
+  });
 
   // Takes in a property name and the event to update local state.
-  handleChange = property => event => {
-    this.setState({
-      ...this.state,
-      [property]: event.target.value
-    });
+  const handleChange = property => event => {
+    setSearchValues({ ...searchValues, [property]: event.target.value });
   };
 
-  render() {
-    return (
-      <>
+  return (
+    <>
+      <Grid
+        container
+        spacing={3}
+        direction="row"
+        justify="flex-start"
+        alignItems="flex-start"
+      >
+        {/* Button to add a new city */}
         <Grid
           container
           spacing={3}
-          direction="column"
+          direction="row"
           justify="center"
-          alignItems="flex-start"
+          alignItems="center"
         >
-          {/* Button to add a new city */}
-          {/* <Grid item xs={6}>
-            <Button variant="contained">Add New City</Button>
-          </Grid> */}
-
-          {/* Input field to search city */}
-          <Grid
-            container
-            spacing={3}
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item sm={6} >
-              <TextField
-                id="city-search-input"
-                label="City"
-                value={this.state.city}
-                onChange={this.handleChange("city")}
-                margin="normal"
-                variant="outlined"
-                fullWidth
-              />
-            </Grid>
-            <Grid item sm={2}>
-              <Button variant="contained" fullWidth>Search</Button>
-            </Grid>
-          </Grid>
-
-          {/* Input field to search cities in a specific country */}
-          <Grid
-            container
-            spacing={3}
-            direction="row"
-            justify="center"
-            alignItems="center"
-          >
-            <Grid item sm={6}>
-              <TextField
-                id="country-search-input"
-                label="Country"
-                value={this.state.country}
-                onChange={this.handleChange("country")}
-                margin="normal"
-                variant="outlined"
-                fullWidth
-              />
-            </Grid>
-            <Grid item sm={2}>
-              <Button variant="contained" fullWidth>Search</Button>
-            </Grid>
+          <Grid item xs={8}>
+            <Button fullWidth className={classes.addButton} variant="contained">
+              Add New City
+            </Button>
           </Grid>
         </Grid>
-        <pre>{JSON.stringify(this.state, null, 2)}</pre>
-      </>
-    );
-  }
+
+        {/* Input field to search city */}
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item sm={6}>
+            <TextField
+              id="city-search-input"
+              label="City"
+              value={searchValues.city}
+              onChange={handleChange("city")}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item sm={2}>
+            <Button variant="contained" fullWidth className={classes.searchButton}>
+              Search
+            </Button>
+          </Grid>
+        </Grid>
+
+        {/* Input field to search cities in a specific country */}
+        <Grid
+          container
+          spacing={3}
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <Grid item sm={6}>
+            <TextField
+              id="country-search-input"
+              label="Country"
+              value={searchValues.country}
+              onChange={handleChange("country")}
+              margin="normal"
+              variant="outlined"
+              fullWidth
+            />
+          </Grid>
+          <Grid item sm={2}>
+            <Button variant="contained" fullWidth className={classes.searchButton}>
+              Search
+            </Button>
+          </Grid>
+        </Grid>
+      </Grid>
+      <pre>{JSON.stringify(searchValues, null, 2)}</pre>
+    </>
+  );
 }
 
 export default CitiesPage;
