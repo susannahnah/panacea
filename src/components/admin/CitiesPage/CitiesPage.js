31c0d6023191638118641108f3f098761f8e12d1
John Gellert <gellertjm@gmail.com>
2019-08-02 11:19:58 -0500

    Updated material ui styles.

--- src/components/admin/CitiesPage/CitiesPage.js
+++ src/components/admin/CitiesPage/CitiesPage.js
@@ -1,24 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-import AdminLayout from '../../layouts/AdminLayout/AdminLayout';
+import AdminLayout from "../../layouts/AdminLayout/AdminLayout";
 
 // Material-UI
 import Button from "@material-ui/core/Button";
-import { TextField } from "@material-ui/core";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
-import { blue } from "@material-ui/core/colors";
+import { TextField, Grid, makeStyles, Paper } from "@material-ui/core";
+import {
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell
+} from "@material-ui/core";
 
 const useStyles = makeStyles({
+  root: {
+    flexGrow: 2
+  },
   addButton: {
-    height: 75,
+    marginTop: 30,
     color: "white",
-    background: "#76a3d5",
+    background: "#76a3d5"
   },
   searchButton: {
     height: 50,
     color: "white",
-    background: "#76a3d5",
+    background: "#76a3d5"
+  },
+  detailsButton: {
+    height: 30,
+    color: "white",
+    background: "#76a3d5"
+  },
+  table: {
+    // minWidth: 800,
+  },
+  paper: {
+    width: "100%",
+    marginTop: 40,
+    overflowX: "auto"
   }
 });
 
@@ -36,84 +57,119 @@ function CitiesPage() {
     setSearchValues({ ...searchValues, [property]: event.target.value });
   };
 
+  const handleChangeDetails = () => {};
+
   return (
     <AdminLayout>
-      <Grid
-        container
-        spacing={3}
-        direction="row"
-        justify="flex-start"
-        alignItems="flex-start"
-      >
-        {/* Button to add a new city */}
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <Grid item xs={8}>
+      <Grid container>
+        <Grid container item spacing={1} direction="row" alignItems="center">
+          <Grid item>
             <Button fullWidth className={classes.addButton} variant="contained">
               Add New City
             </Button>
           </Grid>
         </Grid>
-
-        {/* Input field to search city */}
         <Grid
           container
-          spacing={6}
+          item
+          item
+          spacing={1}
           direction="row"
-          justify="center"
           alignItems="center"
         >
-          <Grid item sm={6}>
+          <Grid item>
             <TextField
               id="city-search-input"
               label="City"
-              value={searchValues.city}
+              value={searchValues.country}
               onChange={handleChange("city")}
               margin="normal"
               variant="outlined"
               fullWidth
             />
           </Grid>
-          <Grid item sm={2}>
-            <Button variant="contained" fullWidth className={classes.searchButton}>
-              Search
-            </Button>
-          </Grid>
-        </Grid>
-
-        {/* Input field to search cities in a specific country */}
-        <Grid
-          container
-          spacing={3}
-          direction="row"
-          justify="center"
-          alignItems="center"
-        >
-          <Grid item sm={6}>
-            <TextField
-              id="country-search-input"
-              label="Country"
-              value={searchValues.country}
-              onChange={handleChange("country")}
-              margin="normal"
-              variant="outlined"
+          <Grid item>
+            <Button
+              variant="contained"
               fullWidth
-            />
-          </Grid>
-          <Grid item sm={2}>
-            <Button variant="contained" fullWidth className={classes.searchButton}>
+              className={classes.searchButton}
+            >
               Search
             </Button>
           </Grid>
         </Grid>
       </Grid>
+      <Grid container item spacing={1} direction="row" alignItems="center">
+        <Grid item>
+          <TextField
+            id="country-search-input"
+            label="Country"
+            value={searchValues.country}
+            onChange={handleChange("country")}
+            margin="normal"
+            variant="outlined"
+            fullWidth
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            variant="contained"
+            fullWidth
+            className={classes.searchButton}
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+      <Grid container item spacing={1} direction="row" alignItems="center">
+      <Grid container item>
+          <Paper className={classes.paper}>
+            <Table className={classes.table}>
+              <TableHead>
+                <TableRow>
+                  <TableCell>City</TableCell>
+                  <TableCell align="right">Country</TableCell>
+                  <TableCell />
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                <TableRow>
+                  <TableCell>Krakow</TableCell>
+                  <TableCell align="right">Poland</TableCell>
+                  <TableCell>
+                    <Link to="/cities/new">
+                      <Button
+                        variant="contained"
+                        fullWidth
+                        className={classes.detailsButton}
+                      >
+                        Details
+                      </Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+                <TableRow>
+                  <TableCell>Warsaw</TableCell>
+                  <TableCell align="right">Poland</TableCell>
+                  <TableCell>
+                    <Link to="/cities/new">
+                      <Button
+                        variant="contained"
+                        fullWidth
+                        className={classes.detailsButton}
+                      >
+                        Details
+                      </Button>
+                    </Link>
+                  </TableCell>
+                </TableRow>
+              </TableBody>
+            </Table>
+          </Paper>
+        </Grid>
+      </Grid>
       <pre>{JSON.stringify(searchValues, null, 2)}</pre>
-      </AdminLayout>
+    </AdminLayout>
   );
 }
 
