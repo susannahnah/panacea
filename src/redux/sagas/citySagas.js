003fd7045b0179c684796eedd620378eb8f81bf8
Susannah Harris <harris.susannah@gmail.com>
2019-08-02 11:00:51 -0500

    do saga CRUD commands for cities

--- src/redux/sagas/citySagas.js
+++ src/redux/sagas/citySagas.js
@@ -13,6 +13,12 @@ function* fetchCitiesSaga(action) {
     }
 };
 
+//GET specific city
+function* selectCitySaga(action) {
+    const getCity = yield axios.get(`/api/cities/${action.payload}`)
+    yield put({ type: 'SET_INDIVIDUAL_CITY', payload: getCity.data })
+    console.log('end of selectCitySaga');
+  }
 
 //POST new city function:
 function* postCitySaga(action) {
@@ -20,16 +26,38 @@ function* postCitySaga(action) {
     try {
         console.log(action.payload);
         yield axios.post('/api/cities', action.payload);
-        // yield put({ type: 'FETCH_CITIES' })
+        yield put({ type: 'FETCH_CITIES' })
     }
     catch (error) {
         console.log('Error with POST', error);
     }
 }
 
+//UPDATE specific city
+function* editCitySaga(action) {
+    yield axios.put(`/api/cities`, action.payload)
+    yield put({ type: 'FETCH_CITIES'})
+}
+
+function* deleteCitySaga(action) {
+    console.log('deleteCitySaga hit')
+    try {
+      yield axios.delete(`/api/cities/${action.payload}`)
+      yield put({type: 'FETCH_CITIES'})
+    } catch (error) {
+      console.log(error);
+      alert('Unable to delete item');
+    }
+}
+
+
 //ALL cities Sagas
 function* citySagas() {
+    yield takeEvery('FETCH_CITIES', fetchCitiesSaga)
+    yield takeEvery('SELECT_CITY', selectCitySaga)
     yield takeEvery('POST_CITY', postCitySaga)
+    yield takeEvery('EDIT_CITY', editCitySaga)
+    yield takeEvery('DELETE_CITY', deleteCitySaga)
 }
 
 export default citySagas;
