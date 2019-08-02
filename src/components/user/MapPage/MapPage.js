06cd19998c530996d256e335b3991bdaf198d87a
Koua Xiong <kxiong517@gmail.com>
2019-08-02 13:18:29 -0500

    working on map feature user side

--- src/components/user/MapPage/MapPage.js
+++ src/components/user/MapPage/MapPage.js
@@ -1,11 +1,46 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 class MapPage extends Component {
 
+  constructor(props){
+    this.props = props
+  }
+
+  static defaultProps = {
+    center: {
+      lat: 50.0647,
+      lng: 19.9450
+    },
+    zoom: 13
+  };
+
   render() {
     return (
-        <></>
-  )}
+      <>
+        <div style={{ height: '100vh', width: '100%' }}>
+          <GoogleMapReact
+            bootstrapURLKeys={{
+              key: '',
+              language: 'en'
+            }}
+            defaultCenter={this.props.center}
+            defaultZoom={this.props.zoom}
+          >
+
+            <AnyReactComponent
+              lat={50.049683}
+              lng={19.944544}
+              text="My Marker"
+            />
+
+          </GoogleMapReact>
+        </div>
+      </>
+    )
+  }
 }
 
 export default (MapPage);
