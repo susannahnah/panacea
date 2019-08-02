6e6d1f739b3738b24d706fe34a862ebdc1bec610
Susannah Harris <harris.susannah@gmail.com>
2019-08-02 10:54:51 -0500

    has saga to get all books

--- src/redux/sagas/citySagas.js
+++ src/redux/sagas/citySagas.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 import { put, takeEvery } from 'redux-saga/effects';
 
 //GET all cities function:
-
+function* fetchCitiesSaga(action) {
+    try {
+        const cityResponse = yield axios.get('/api/cities')
+        console.log('cool cities brah', cityResponse);
+        yield put({ type: 'SET_CITIES', payload: cityResponse.data });
+        console.log('end of fetchCitiesSaga');
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 
 //POST new city function:
