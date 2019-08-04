53bdab479921d20d9fa84f7c3861bd6b6bece92c
Susannah Harris <harris.susannah@gmail.com>
2019-08-04 15:22:59 -0500

    individualOrgReducer completed for getting individual orgs

--- src/redux/reducers/individualOrgReducer.js
+++ src/redux/reducers/individualOrgReducer.js
@@ -1,3 +1,23 @@
 const orgDetails = {
-    
+    "id": 0,
+    "name":'',
+    "type": '',
+    "recommended": '',
+    "twentyfour": '',
+    "hours": '',
+    "homeopathic_remedies": '',
+    "labor_delivery": '',
+    "childrens": '',
+    "childrens_surgery": '',
+    "adult": '',
+    "adult_surgery": '',
+    "medical_translators": '',
+    "comments": '',
+    "phone_number": '',
+    "website_url": '',
+    "lat": '',
+    "long": '',
+    "google_maps_link": '',
+
+
 }
