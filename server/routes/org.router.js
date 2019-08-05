9d4a106b2a660839c245dd5f5a2b40649e4b0ca2
Susannah Harris <harris.susannah@gmail.com>
2019-08-05 09:26:25 -0500

    finished org router commands.  updated org and city router and commented out get all router commands

--- server/routes/org.router.js
+++ server/routes/org.router.js
@@ -4,17 +4,17 @@ const { rejectUnauthenticated } = require('../modules/authentication-middleware'
 const router = express.Router();
 
 //GET all orgs
-router.get('/', (req, res) => {
-    const queryText = 'SELECT * FROM "organizations" ORDER BY "id"';
-    pool.query(queryText)
-        .then((result) => {
-            res.send(result.rows);
-        })
-        .catch((error) => {
-            console.log('Error completely SELECT org query', error)
-            res.sendStatus(500)
-        })
-})
+// router.get('/', (req, res) => {
+//     const queryText = 'SELECT * FROM "organizations" ORDER BY "id"';
+//     pool.query(queryText)
+//         .then((result) => {
+//             res.send(result.rows);
+//         })
+//         .catch((error) => {
+//             console.log('Error completely SELECT org query', error)
+//             res.sendStatus(500)
+//         })
+// })
 
 
 //GET selected orgs
@@ -54,25 +54,29 @@ router.post('/', rejectUnauthenticated, (req, res) => {
     "website_url"=$15,
     "lat"=$16,
     "long"=$17,
-    "google_maps_link"=418,
+    "google_maps_link"=$18,
     ) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11,
              $12, $13, $14, $15, $16, $17, $18) 
     RETURNING "id"`;
     const queryValues = [
         newOrg.name,
-        newOrg.overview,
-        newOrg.health_risks,
-        newOrg.ambulance,
-        newOrg.fire,
-        newOrg.police,
-        newOrg.roadside_assistance,
-        newOrg.wellness_resources,
-        newOrg.local_health_remedies,
-        newOrg.healthcare_tourism,
-        newOrg.WHO_link,
-        newOrg.CDC_link,
-        newOrg.google_translate_link,
-        newOrg.local_resources
+        newOrg.type,
+        newOrg.recommend,
+        newOrg.twentyfour,
+        newOrg.hours,
+        newOrg.homeopathic_remedies,
+        newOrg.labor_delivery,
+        newOrg.childrens,
+        newOrg.childrens_surgery,
+        newOrg.adult,
+        newOrg.adult_surgery,
+        newOrg.medical_translators,
+        newOrg.comments,
+        newOrg.phone_number,
+        newOrg.website_url,
+        newOrg.lat,
+        newOrg.long,
+        newOrg.google_maps_link
     ];
     pool.query(queryText, queryValues)
         .then((result) => {
@@ -80,68 +84,75 @@ router.post('/', rejectUnauthenticated, (req, res) => {
             console.log(result);
         })
         .catch((error) => {
-            console.log('Error completing POST city query', error);
+            console.log('Error completing POST org query', error);
             res.sendStatus(500);
         });
 });
 
 
-//UPDATE city
+//UPDATE org
 router.put('/', rejectUnauthenticated, (req, res) => {
     console.log('testtest', req.body);
     const updateCity = req.body;
-    pool.query(`UPDATE "cities"
+    pool.query(`UPDATE "organizations"
     SET 
-    "name"=$1, 
-    "overview"=$2, 
-    "health_risks"=$3,
-    "ambulance"=$4,
-    "fire"=$5, 
-    "police"=$6, 
-    "roadside_assistance"=$7, 
-    "wellness_resources"=$8, 
-    "local_health_remedies"=$9, 
-    "healthcare_tourism"=$10,
-    "WHO_link"=$11,
-    "CDC_link"=$12,
-    "google_translate_link"=$13,
-    "local_resources"=$14
-    WHERE "id"=$15;`,
+    "name"=$1,
+    "type"=$2,
+    "recommended"=$3,
+    "twentyfour"=$4,
+    "hours"=$5,
+    "homeopathic_remedies"=$6,
+    "labor_delivery"=$7,
+    "childrens"=$8,
+    "childrens_surgery"=$9,
+    "adult"=$10,
+    "adult_surgery"=$11,
+    "medical_translators"=$12,
+    "comments"=$13,
+    "phone_number"=$14,
+    "website_url"=$15,
+    "lat"=$16,
+    "long"=$17,
+    "google_maps_link"=$18,
+    WHERE "id"=$19;`,
         [
-            updatedCity.name,
-            updatedCity.overview,
-            updatedCity.health_risks,
-            updatedCity.ambulance,
-            updatedCity.fire,
-            updatedCity.police,
-            updatedCity.roadside_assistance,
-            updatedCity.wellness_resources,
-            updatedCity.local_health_remedies,
-            updatedCity.healthcare_tourism,
-            updatedCity.WHO_link,
-            updatedCity.CDC_link,
-            updatedCity.google_translate_link,
-            updatedCity.local_resources,
-            updatedCity.id
+            updatedOrg.name,
+            updatedOrg.type,
+            updatedOrg.recommend,
+            updatedOrg.twentyfour,
+            updatedOrg.hours,
+            updatedOrg.homeopathic_remedies,
+            updatedOrg.labor_delivery,
+            updatedOrg.childrens,
+            updatedOrg.childrens_surgery,
+            updatedOrg.adult,
+            updatedOrg.adult_surgery,
+            updatedOrg.medical_translators,
+            updatedOrg.comments,
+            updatedOrg.phone_number,
+            updatedOrg.website_url,
+            updatedOrg.lat,
+            updatedOrg.long,
+            updatedOrg.google_maps_link
         ]
     )
         .then((response) => {
             res.sendStatus(200)
         })
         .catch((error) => {
-            console.log('ERROR completing UPDATE of city', error);
+            console.log('ERROR completing UPDATE of org', error);
             res.sendStatus(500)
         });
 });
 
 
-//DELETE city 
+//DELETE org 
 router.delete('/:id', rejectUnauthenticated, (req, res) => {
-    const queryText = 'DELETE FROM "cities" WHERE id=$1';
+    const queryText = 'DELETE FROM "organizations" WHERE id=$1';
     pool.query(queryText, [req.params.id])
         .then(() => { res.sendStatus(200); })
         .catch((err) => {
-            console.log('Error completing DELETE city query', err);
+            console.log('Error completing DELETE org query', err);
             res.sendStatus(500);
         });
 });
