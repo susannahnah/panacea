c82adcf543f7076279f49080fa403b183a27a6c5
dayton brock <dayton.brock@gmail.com>
2019-08-09 09:11:27 -0500

    format city page, hospital icon

--- src/components/user/CityPage/CityPage.js
+++ src/components/user/CityPage/CityPage.js
@@ -2,8 +2,23 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './CityPage.css';
-import { Grid, Paper, Typography, Link as MuiLink, Card } from '@material-ui/core/';
+import hospitalIcon from '../../../images/hospital-teal.png';
 import UserLayout from '../../layouts/UserLayout/UserLayout';
+
+// Material-UI components
+import { 
+  Grid, 
+  Paper, 
+  Typography,
+  Link as MuiLink, 
+  Card, 
+  SvgIcon,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails
+} from '@material-ui/core/';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
 
 class CityPage extends Component {
 
@@ -27,112 +42,136 @@ class CityPage extends Component {
   render() {
     return (
       <UserLayout>
-        <Paper style={{width: `100%`, paddingLeft: `5%`}} square={true}>
-          <Typography variant='h5' gutterBottom style={{ marginTop: '2%', marginBottom: '2%' }}>
+        <Paper style={{width: `100%` }} square={true}>
+          <Typography variant='h5' gutterBottom style={{ paddingLeft: `4%`, marginTop: '2%', marginBottom: '2%' }}>
             <b>{this.state.city.city_name}, {this.state.city.city_country_id}</b>
           </Typography>
+          <div className="stock-map">
+            <Grid
+              container
+              spacing={1}
+            >
+
+              {this.state.orgTypes.map(
+                (type, i) => (
+
+                  <Grid
+                    key={i}
+                    item xs={6}
+                    style={{ textAlign: `center`, marginTop: `3vh` }}
+                  >
+
+                    <Link
+                      style={{ display: 'block' }}
+                      to={{
+                        pathname: `/map/${this.props.match.params.cityName}`,
+                        city_id: this.state.city.city_id,
+                        orgType: type,
+                        coordinates: {
+                          lat: Number(this.state.city.lat),
+                          lng: Number(this.state.city.long)
+                        },
+                      }}>
+                      <Card style={{width: `35vw`, margin: `auto`, opacity: `.8`, elevation: `12`}}>
+                        <Typography style={{ color: `#2ECBB0` }}>
+                          {type}
+                        </Typography>
+                      </Card>
+                      
+                    </Link>
+
+                  </Grid>
+
+                ))}
+
+            </Grid>
+          </div>
+          <Typography variant='h6' gutterBottom style={{ paddingLeft: `3%`, marginTop: '2%', marginBottom: '2%' }}>
+            <img src={hospitalIcon} alt="Hospital Icon"/> 
+            <> Healthcare in the City</>
+          </Typography>
         </Paper>
-        <div className="stock-map">
-          <Grid
-            container
-            spacing={1}
-          >
-
-            {this.state.orgTypes.map(
-              (type, i) => (
-
-                <Grid
-                  key={i}
-                  item xs={6}
-                  style={{ textAlign: `center`, marginTop: `4vh` }}
-                >
-
-                  <Link
-                    style={{ display: 'block' }}
-                    to={{
-                      pathname: `/map/${this.props.match.params.cityName}`,
-                      city_id: this.state.city.city_id,
-                      orgType: type,
-                      coordinates: {
-                        lat: Number(this.state.city.lat),
-                        lng: Number(this.state.city.long)
-                      },
-                    }}>
-                    <Card style={{width: `40vw`, margin: `auto`}}>
-                      <Typography>
-                        {type}
-                      </Typography>
-                    </Card>
-                    
-                  </Link>
-
-                </Grid>
-
-              ))}
-
-          </Grid>
-        </div>
-        <Typography variant='h6' gutterBottom style={{ marginTop: '2%', marginBottom: '2%' }}>
-          Healthcare in the city
-        </Typography>
-        {/* <div style={{ marginTop: '2%', marginBottom: '2%' }}>
-          <b>Healthcare in the city</b>
-        </div> */}
-
         <Grid item xs={12}>
-
-          <Paper style={{ backgroundColor: `#F8F9FA`, padding: `2% 2%` }} square={true}>
+          <Paper style={{ backgroundColor: `#F8F9FA`}} square={true}>
+          {/* <ExpansionPanel style={{width: `100%`}}>
+            <ExpansionPanelSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>Expansion Panel 1</Typography>
+            </ExpansionPanelSummary>
+            <ExpansionPanelDetails>
+              <Typography>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
+                sit amet blandit leo lobortis eget.
+              </Typography>
+            </ExpansionPanelDetails>
+          </ExpansionPanel> */}
 
             {/* TODO: Refactor this code */}
 
-            <Typography variant='h5' gutterBottom> 
-              Overview: <Typography variant='body1' gutterBottom display='inline'>{this.state.city.overview}</Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Overview:</b>
             </Typography>
-
-            <Typography variant='h5' gutterBottom>
-              Health Risks: <Typography variant='body1' gutterBottom display='inline'>{this.state.city.health_risks}</Typography>
+              <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                {this.state.city.overview}
+              </Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Health Risks:</b>
             </Typography>
-
-            <Typography variant='h5' gutterBottom>
-              Wellness Resources: <Typography variant='body1' gutterBottom display='inline'>{this.state.city.wellness_resources}</Typography>
+              <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                {this.state.city.health_risks}
+              </Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Wellness Resources:</b>
             </Typography>
-
-            <Typography variant='h5' gutterBottom>
-              Local Health Remedies: <Typography variant='body1' gutterBottom display='inline'>{this.state.city.local_health_remedies}</Typography>
+              <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                {this.state.city.wellness_resources}
+              </Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Local Health Remedies</b>
             </Typography>
-
-            <Typography variant='h5' gutterBottom>
-              Healthcare Tourism: <Typography variant='body1' gutterBottom display='inline'>{this.state.city.healthcare_tourism}</Typography>
+              <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                {this.state.city.local_health_remedies}
+              </Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Healthcare Tourism:</b>
             </Typography>
-            
-            {this.state.city.WHO_link &&
-            <Typography gutterBottom>
-              <MuiLink href={this.state.city.WHO_link} target="_blank" underline='always'>
-                WHO Link
-              </MuiLink>
-            </Typography>}
-
-            {this.state.city.CDC_link &&
-            <Typography gutterBottom>
-              <MuiLink href={this.state.city.CDC_link} target="_blank" underline='always'>
-                CDC Link
-              </MuiLink>
-            </Typography>}
-
-            {this.state.city.google_translate_link &&
-            <Typography gutterBottom>
-              <MuiLink href={this.state.city.google_translate_link} target="_blank" underline='always'>
-              Google Translate Link
-              </MuiLink>
-            </Typography>}
-
-            {this.state.city.local_resources &&
-            <Typography gutterBottom>
-              <MuiLink href={this.state.city.local_resources} target="_blank" underline='always'>
-              Local Resources
-              </MuiLink>
-            </Typography>}
-
+              <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                {this.state.city.healthcare_tourism}
+              </Typography>
+            <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+              <b>Important Resources:</b>
+            { this.state.city.WHO_link &&
+              <Typography gutterBottom>
+                <MuiLink href={this.state.city.WHO_link} target="_blank" underline='always'>
+                  WHO Link
+                </MuiLink>
+              </Typography>
+            }
+            { this.state.city.CDC_link &&
+              <Typography gutterBottom>
+                <MuiLink href={this.state.city.CDC_link} target="_blank" underline='always'>
+                  CDC Link
+                </MuiLink>
+              </Typography>
+            }
+            { this.state.city.google_translate_link &&
+              <Typography gutterBottom>
+                <MuiLink href={this.state.city.google_translate_link} target="_blank" underline='always'>
+                Google Translate Link
+                </MuiLink>
+              </Typography>
+            }
+            { this.state.city.local_resources &&
+              <Typography gutterBottom>
+                <MuiLink href={this.state.city.local_resources} target="_blank" underline='always'>
+                Local Resources
+                </MuiLink>
+              </Typography>
+            }
+            </Typography>
             <Link 
             to={{ pathname: `/city/${this.props.match.params.cityName}/medications` }}
             style={{ textDecoration: 'underline', color: 'blue'}}
