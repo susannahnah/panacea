5da5b6600cb782209aecc7601267bd4498bbd1f7
dayton brock <dayton.brock@gmail.com>
2019-08-10 11:09:58 -0500

    style city page content

--- src/components/user/CityPage/CityPage.js
+++ src/components/user/CityPage/CityPage.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import './CityPage.css';
 import hospitalIcon from '../../../images/hospital-teal.png';
+import phoneIcon from '../../../images/Ringing-phone-teal.png';
 import UserLayout from '../../layouts/UserLayout/UserLayout';
 
 // Material-UI components
@@ -75,7 +76,7 @@ class CityPage extends Component {
                       <Card style={{
                           width: `35vw`, 
                           margin: `auto`, 
-                          opacity: `.9`, 
+                          opacity: `.8`, 
                           elevation: `12`, 
                           backgroundColor: `#4A8CCD`,
                           padding: `2.5%`
@@ -94,7 +95,7 @@ class CityPage extends Component {
             </Grid>
           </div>
           <Typography variant='h6' gutterBottom style={{ paddingLeft: `3%`, marginTop: '2%', marginBottom: '2%' }}>
-            <img src={hospitalIcon} alt="Hospital Icon"/> 
+            <img src={hospitalIcon} alt="Hospital Icon" /> 
             <> Healthcare in the City</>
           </Typography>
         </Paper>
@@ -107,10 +108,10 @@ class CityPage extends Component {
                 id="panel1a-header"
               >
                 <div>
-                  <Typography variant='body2' gutterBottom style={{ paddingLeft: `8%`, paddingRight: `8%` }}>
+                  <Typography variant='body2' gutterBottom style={{ paddingLeft: `6%`, paddingRight: `6%` }}>
                     <b>Overview:</b> 
                   </Typography>
-                    <Typography variant='body2' gutterBottom style={{ paddingLeft: `9%`, paddingRight: `8%` }}>
+                    <Typography variant='body2' gutterBottom style={{ paddingLeft: `7%`, paddingRight: `6%` }}>
                       {this.state.city.overview}
                     </Typography>
                 </div>
@@ -177,45 +178,35 @@ class CityPage extends Component {
             </ExpansionPanel>
           </Paper>  
         </Grid>
-
-        <Typography variant='h4' gutterBottom style={{ marginTop: '2%', marginBottom: '2%' }}>
-          <b>Emergency Phone Numbers</b>
+        <Typography variant='h6' gutterBottom style={{ paddingLeft: `3%`, marginTop: '4%', marginBottom: '2%' }}>
+            <img src={phoneIcon} alt="Phone Icon" style={{ width: `6%` }}/> 
+            <> Emergency Phone Numbers</>
         </Typography>
-
         <Grid item xs={12}>
-
-          <Paper style={{ backgroundColor: `#F8F9FA`, padding: `2% 2%` }} square={true}>
-
-            {/* TODO: Refactor this code */}
-
-            <Typography gutterBottom>
-              Fire: {this.state.city.fire}
+          <Paper style={{ backgroundColor: `#F8F9FA`, padding: `2% 11%` }} square={true}>
+            <Typography variant='body1' gutterBottom>
+              {this.state.city.fire}: Fire
             </Typography>
-
-            <Typography gutterBottom>
-              Police: {this.state.city.police}
+            <Typography variant='body1' gutterBottom>
+              {this.state.city.police}: Police
             </Typography>
-
-            <Typography gutterBottom>
-              Ambulance: {this.state.city.ambulance}
+            <Typography variant='body1' gutterBottom>
+              {this.state.city.ambulance}: Ambulance
             </Typography>
-
-            <Typography>
-              Roadside Assist: {this.state.city.roadside_assistance}
+            <Typography variant='body1' gutterBottom>
+              {this.state.city.roadside_assistance}: Roadside Assist
             </Typography>
-
           </Paper>
-
         </Grid>
-        {/* <Link 
+        <Link 
         to={{ pathname: `/city/${this.props.match.params.cityName}/medications` }}
-        style={{ textDecoration: 'underline', color: 'blue'}}
+        style={{ textDecoration: 'underline', color: 'blue', padding: `11%`}}
         >
           <Typography>
             Medicine Translations
           </Typography>
           
-        </Link> */}
+        </Link>
         {/* <pre>
           {JSON.stringify(this.state, null, 2)}
         </pre>
